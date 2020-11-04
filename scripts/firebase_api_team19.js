f2241d093bd6d555dfb7ca9fc6e04cb70993377d
stevensjcho <stevensjcho@gmail.com>
2020-11-03 21:16:35 -0800

    connected the website with a firebase db and embedded the authentication widget. I also made a login.html and added a nav bar to index.html, login.html, and main.html.

---
+++ scripts/firebase_api_team19.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC6mQ8KS4GkD7f7BwlQZKoorX6PVRpAqj0",
+    authDomain: "team19proje.firebaseapp.com",
+    databaseURL: "https://team19proje.firebaseio.com",
+    projectId: "team19proje",
+    storageBucket: "team19proje.appspot.com",
+    messagingSenderId: "265077749831",
+    appId: "1:265077749831:web:c1415dba8a554da5795962"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
