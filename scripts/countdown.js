f87d8a4ee507ab21862a7c1158e1830835d89a41
John Braithwaite <jwbraith@gmail.com>
2020-11-04 22:06:44 -0800

    more countdown changes

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -2,7 +2,6 @@ let countDownLength = new Date().getSeconds() + 60;
 let x = setInterval(function() {
   let now = new Date().getSeconds();
   let distance = countDownLength - now;
-  let seconds = Math.floor(distance);
-
-  document.getElementById("count").innerHTML = seconds + "seconds";
+  let seconds = distance;
+  document.getElementById("count").innerHTML = seconds;
 }, 1000);
