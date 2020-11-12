ad8c9e043032abe4c6212ed099f8aa62df5702d4
John Braithwaite <jwbraith@gmail.com>
2020-11-12 13:25:48 -0800

    made an invite and a play with page

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,8 @@
-
+var roomsRef = db.collection("rooms");
+function createRoom() {
+  console.log("create Room function called");
+  roomsRef.doc("room1").set({
+    name: "room1",
+    inviteID: "8945"
+  })
+}
